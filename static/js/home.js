53e0b49c63f720b8b0132a53032a3f96ad209658
GUILDER <pplg39395@gmail.com>
2026-01-14 14:19:23 -0400

    Implementacion del Home

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     // home.js
-    // Lógica específica para la página de inicio (si fuera necesaria)
+    // Lógica específica para la página de inicio
+
+    // Botón CTA de registro
+    const openRegistroModalCta = document.getElementById('open-registro-modal-cta');
+    if (openRegistroModalCta) {
+        openRegistroModalCta.addEventListener('click', () => {
+            const openRegistroModal = document.getElementById('open-registro-modal');
+            if (openRegistroModal) {
+                openRegistroModal.click();
+            }
+        });
+    }
+
     // Los modales de login y registro son manejados por sus propios scripts.
 });
