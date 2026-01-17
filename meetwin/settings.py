b1ef7f27748d453d9d7ffc69f90c4c0af1a3bacf
GUILDER <pplg39395@gmail.com>
2026-01-17 16:00:38 -0400

    Configurar ALLOWED_HOSTS para Render

--- meetwin/settings.py
+++ meetwin/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-w(q(abp9d%t$c&f-(k0el
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'sistema-meppred-6.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
