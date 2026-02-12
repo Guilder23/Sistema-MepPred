be798f5fa5ee85357bb772b22fbae8768abd2717
GUILDER <pplg39395@gmail.com>
2026-02-12 18:46:16 -0400

    Ya funciona y filtra de manera correcta en RANKING

--- apps/ranking/urls.py
+++ apps/ranking/urls.py
@@ -6,4 +6,5 @@ app_name = 'ranking'
 urlpatterns = [
     path('', views.ranking_estudiantes, name='ranking'),
     path('mi-posicion/', views.mi_posicion, name='mi_posicion'),
+    path('api/filtros/', views.api_filtros_ranking, name='api_filtros'),
 ]
